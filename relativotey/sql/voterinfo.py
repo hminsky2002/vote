08c37a56ccd67bce82c36b4737974c6b87e8a88d
hminsky <henry.minsky@gmail.com>
2018-09-14 19:35:35 -0400

    set default database in sql connection

--- relativotey/sql/voterinfo.py
+++ relativotey/sql/voterinfo.py
@@ -1,41 +1,23 @@
 # Copyright 2013, 2018 Beartronics Inc. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 """
 Sample App Engine application demonstrating how to connect to Google Cloud SQL
 using App Engine's native unix socket or using TCP when running locally.
 
-For more information, see the README.md.
 """
 
 # [START gae_python_mysql_app]
 import os
 
-#import pymysql
-#pymysql.install_as_MySQLdb()
-
-
 import MySQLdb
 import webapp2
 import json
-
 
 # These environment variables are configured in app.yaml.
 CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
 CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
-
 
 def connect_to_cloudsql():
     # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
@@ -49,7 +31,7 @@ def connect_to_cloudsql():
         db = MySQLdb.connect(
             unix_socket=cloudsql_unix_socket,
             user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD)
+            passwd=CLOUDSQL_PASSWORD, db='voter')
 
     # If the unix socket is unavailable, then try to connect using TCP. This
     # will work if you're running a local MySQL server or using the Cloud SQL
@@ -59,34 +41,32 @@ def connect_to_cloudsql():
     #
     else:
         db = MySQLdb.connect(
-            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD, db='voter')
 
     return db
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        """Simple request handler that shows all of the MySQL variables."""
+        """Makes a SQL query to voter db, returns result as JSON"""
         self.response.headers['Content-Type'] = 'text/plain'
+
         try:
             db = connect_to_cloudsql()
             cursor = db.cursor()
-
             town = self.request.get('town')
-            self.response.write(town)
-            return
-            cursor.execute('select sum(Registered_Voters) as vcount from voter.voters where town = %s', town)
-            
-            if cursor.rowcount > 0:
-                r = cursor.fetchone()
-                self.response.write('{}'.format(json.dumps(r)))
+            # python tuple syntax sux, that's why you need "(town,)" to make a single element tuple
+            cursor.execute('SELECT sum(Registered_Voters) as vcount FROM voters WHERE town=%s', (town,))
 
-            else:
-                self.response.write('no_ items_found')
-        
-        except MySQLdb.Error, e:
+            r = cursor.fetchone()
+            # you get back a "Decimal" object from sql, need to convert to int in order to serialize to JSON
+            vcount = int(r[0])
+            results = {"registered": vcount, "town": town}
+
+            self.response.write(json.dumps(results))
+
+        #except MySQLdb.Error, e:
+        except Exception as e:
             print "Error %d: %s" % (e.args[0], e.args[1])
-
 
 
 app = webapp2.WSGIApplication([
