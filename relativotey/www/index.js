e53bfaee19be9f518dbfbabd9590d658c6d18bdc
hminsky <henry.minsky@gmail.com>
2018-10-21 17:01:54 -0400

    added kentucky, added facebook share button

--- relativotey/www/index.js
+++ relativotey/www/index.js
@@ -252,7 +252,10 @@ function showVoterInfo(stateAbbrev, district) {
 
    var shareVotingPower = `<p class="vote-box-text">Share your voting power:</p>
    <div class="vote-box-icons">
-       <a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a>
+
+
+<div class="fb-share-button" data-href="https://www.relativotey.org" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.relativotey.org%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa fa-facebook"></i></a></div>
+
        <a href="http://www.twitter.com/share?text=${twitter_text}&url=${twitter_url}&hashtags=${hashtags}"><i class="fa fa-twitter-square"></i></a>
    </div>`;
    $('.vote-power-share').html(shareVotingPower);
